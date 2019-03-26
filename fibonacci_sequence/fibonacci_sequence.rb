e329893579773a046312f8d6b53d89010ce652c6
Kevin Vuong <c_k.k@hotmail.com>
2019-03-26 14:33:57 +1100

    fibonacci sequence

---
+++ fibonacci_sequence/fibonacci_sequence.rb
@@ -0,0 +1,16 @@
+def fibs(n)          # returns "n" number of fibonacci numbers, solved through iteration
+  (0...n).each_with_object([]) do |n, array|
+    array << n if (n < 2)
+    array << array[-1] + array[-2] unless (n < 2)
+  end
+end
+
+def fibs_rec(n)      # returns "n" number of fibonacci numbers, solved through recursion
+  return [0, 1][0...n] if n <= 2
+  fib_numbers = fibs_rec(n - 1)
+  fib_numbers << fib_numbers[-1] + fib_numbers[-2]
+end
+
+
+p fibs(100)
+p fibs_rec(0)
